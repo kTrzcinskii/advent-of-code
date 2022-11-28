f1a684ae981bfa955cc9c51d9c43cde2aa6cbbad
kTrzcinskii <kt.trzcinski@gmail.com>
2022-11-28 16:44:29 +0100

    2021-1day-easy

---
+++ 2021/day-1/easy/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int current, prev, counter = 0, incrs = 0;
+    while(scanf("%d", &current))
+    {
+        if (current < 0) break;
+        if (counter == 0) 
+        {
+            counter++;
+            prev = current;
+            continue;
+        }
+
+        if (current > prev) incrs++;
+        prev = current;
+        counter++;
+    }
+    printf("icreases: %d", incrs);
+}
