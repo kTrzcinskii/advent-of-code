a1dcf9033a8400c37cb4d56a1cb72cbc93d5c910
kTrzcinskii <kt.trzcinski@gmail.com>
2024-01-19 00:18:15 +0100

    2023 - Day 4 part 2

--- 2023/2023/Day_4.cs
+++ 2023/2023/Day_4.cs
@@ -6,16 +6,40 @@
         private static readonly string testPath = "./Tests/Day_4.txt";
         private static readonly int pointsBase = 2;
 
-        public static void Solve(bool isTest = false)
+        public static void Solve(int part = 1, bool isTest = false)
         {
             string input = File.ReadAllText(isTest ? testPath : inputPath);
             var lines = input.Split('\n');
 
             int sum = 0;
+            List<int> nextCardCopies = new List<int>();
 
             foreach (var line in lines)
             {
-                sum += CalculatePoints(line);
+                if (part == 1)
+                {
+                    sum += CalculatePoints(line);
+                }
+                else if (part == 2)
+                {
+                    var copies = nextCardCopies.Count > 0 ? nextCardCopies.ElementAt(0) + 1 : 1;
+                    if (nextCardCopies.Count > 0)
+                    {
+                        nextCardCopies.RemoveAt(0);
+                    }
+                    var (count, nextCopies) = GetCardCopies(line, copies);
+                    sum += count;
+                    var arr = nextCardCopies.ToArray();
+                    if (arr.Length < nextCopies.Length)
+                    {
+                        Array.Resize(ref arr, nextCopies.Length);
+                    }
+                    for (int i = 0; i < nextCopies.Length; i++)
+                    {
+                        arr[i] += nextCopies[i];
+                    }
+                    nextCardCopies = arr.ToList();
+                }
             }
 
             Console.WriteLine($"Sum: {sum}");
@@ -41,6 +65,27 @@
             var winningNumbers = Array.ConvertAll(sp[0].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
             var actualNumbers = Array.ConvertAll(sp[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
             return (winningNumbers, actualNumbers);
+        }
+
+        private static (int cardCount, int[] nextCopies) GetCardCopies(string line, int copies)
+        {
+            var (winningNumbers, actualNumbers) = GetNumbers(line);
+
+            int count = winningNumbers.Intersect(actualNumbers).Count();
+            if (count == 0)
+            {
+                return (copies, Array.Empty<int>());
+            }
+
+            int[] arr = new int[count];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = copies;
+            }
+
+            return (copies, arr);
+
         }
     }
 }
--- 2023/2023/Program.cs
+++ 2023/2023/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Day_4.Solve();
+            Day_4.Solve(2);
         }
     }
 }
