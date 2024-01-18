6ae2eb020f2af216e55cea035a097ed441b68395
kTrzcinskii <kt.trzcinski@gmail.com>
2024-01-18 17:46:15 +0100

    2023 - Day 3 Part 2

--- 2023/2023/Day_3.cs
+++ 2023/2023/Day_3.cs
@@ -4,31 +4,38 @@
     {
         private static readonly string inputPath = "./Inputs/Day_3.txt";
         private static readonly string testPath = "./Tests/Day_3.txt";
+        private static readonly char ratioSymbol = '*';
 
-        private struct NumberInText
+        private struct ElementInText
         {
-            public int value;
             public int left;
             public int right;
             public int level;
 
-            public NumberInText(int value, int left, int right, int level)
+            public ElementInText(int left, int right, int level)
             {
-                this.value = value;
                 this.left = left;
                 this.right = right;
                 this.level = level;
             }
         }
 
-        public static void Solve(bool isTest = false)
+        public static void Solve(int part = 1, bool isTest = false)
         {
             string input = File.ReadAllText(isTest ? testPath : inputPath);
-            var lines = input.Split("\n");
-            for (int i = 0; i < lines.Length; i++)
+            var lines = input.Replace("\r", string.Empty).Split("\n");
+            if (part == 1)
             {
-                lines[i] = lines[i].Replace("\r", String.Empty);
+                Part1(lines);
             }
+            else
+            {
+                Part2(lines);
+            }
+        }
+
+        private static void Part1(string[] lines)
+        {
             int sum = 0;
             for (int k = 0; k < lines.Length; k++)
             {
@@ -49,10 +56,10 @@
                             length++;
                         }
                         int value = int.Parse(line.Substring(first, length));
-                        var num = new NumberInText(value, i, i + length - 1, k);
+                        var num = new ElementInText(i, i + length - 1, k);
                         if (IsPartNumber(lines, num))
                         {
-                            sum += num.value;
+                            sum += value;
                         }
                     }
                     i += length;
@@ -62,7 +69,24 @@
             Console.WriteLine($"Sum: {sum}");
         }
 
-        private static bool IsPartNumber(string[] lines, NumberInText num)
+        private static void Part2(string[] lines)
+        {
+            int sum = 0;
+            for (int k = 0; k < lines.Length; k++)
+            {
+                var line = lines[k];
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == ratioSymbol)
+                    {
+                        sum += FindRatio(lines, new ElementInText(i, i, k));
+                    }
+                }
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        private static bool IsPartNumber(string[] lines, ElementInText num)
         {
             int level = num.level;
             bool isLeftWall = num.left == 0;
@@ -109,6 +133,127 @@
         private static bool IsCorrectSymbol(char c)
         {
             return c != '.';
+        }
+
+        private static int FindRatio(string[] lines, ElementInText symbol)
+        {
+            int level = symbol.level;
+            bool isLeftWall = symbol.left == 0;
+            bool isRightWall = symbol.right == lines[level].Length - 1;
+            int startLeft = isLeftWall ? symbol.left + 1 : symbol.left - 1;
+            int rightEnd = isRightWall ? symbol.right - 1 : symbol.right + 1;
+
+            int firstNumber = 0, secondNumber = 0;
+
+            if (!isLeftWall && char.IsDigit(lines[level][symbol.left - 1]))
+            {
+                firstNumber = FindNumberInText(lines[level], symbol.left - 1, out _);
+            }
+
+            if (!isRightWall && char.IsDigit(lines[level][symbol.right + 1]))
+            {
+                int num = FindNumberInText(lines[level], symbol.right + 1, out _);
+                if (firstNumber == 0)
+                {
+                    firstNumber = num;
+                }
+                else
+                {
+                    secondNumber = num;
+                }
+            }
+
+            if (level > 0)
+            {
+                for (int i = startLeft; i <= rightEnd; i++)
+                {
+                    int step = 0;
+                    if (char.IsDigit(lines[level - 1][i]))
+                    {
+                        int num = FindNumberInText(lines[level - 1], i, out step);
+                        if (firstNumber == 0)
+                        {
+                            firstNumber = num;
+                        }
+                        else if (secondNumber == 0)
+                        {
+                            secondNumber = num;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                    i += step;
+                }
+            }
+
+            if (level < lines.Length - 1)
+            {
+                for (int i = startLeft; i <= rightEnd; i++)
+                {
+                    int step = 0;
+                    if (char.IsDigit(lines[level + 1][i]))
+                    {
+                        int num = FindNumberInText(lines[level + 1], i, out step);
+                        if (firstNumber == 0)
+                        {
+                            firstNumber = num;
+                        }
+                        else if (secondNumber == 0)
+                        {
+                            secondNumber = num;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                    i += step;
+                }
+            }
+
+            Console.WriteLine($"Znalezione numery: {firstNumber} - {secondNumber}");
+
+            return firstNumber * secondNumber;
+        }
+
+        private static int FindNumberInText(string line, int start, out int step)
+        {
+            int len = 1;
+            int i = start - 1;
+            int leftLen = 0;
+            while (i >= 0)
+            {
+                if (char.IsDigit(line[i]))
+                {
+                    leftLen++;
+                    i--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            int rightLen = 0;
+            i = start + 1;
+            while (i < line.Length)
+            {
+                if (char.IsDigit(line[i]))
+                {
+                    rightLen++;
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            len += leftLen + rightLen;
+
+            step = rightLen;
+
+            return int.Parse(line.Substring(start - leftLen, len));
         }
     }
 }
--- 2023/2023/Program.cs
+++ 2023/2023/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Day_3.Solve();
+            Day_3.Solve(2);
         }
     }
 }
